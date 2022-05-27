f0978fba7f5e7ad39869fb30b83fe718eb4f0e17
av.litvin <av.litvin73@gmail.com>
2022-05-27 18:32:22 +0300

    fix: worked edit button

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -18,33 +18,42 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
   var listItem=document.createElement("li");
-
+  listItem.classList.add('part')
   //input (checkbox)
   var checkBox=document.createElement("input");//checkbx
+  
   //label
   var label=document.createElement("label");//label
   //input (text)
   var editInput=document.createElement("input");//text
+  
   //button.edit
   var editButton=document.createElement("button");//edit button
 
-  //button.delete
+  //button.del
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
   label.className='task';
+  
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.classList.add('checkbox')
   editInput.type="text";
   editInput.className="task";
+  editInput.classList.add('hide')
+  editInput.classList.add('txt')
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
   editButton.className="edit";
+  editButton.classList.add('btn')
 
-  deleteButton.className="delete";
+  deleteButton.className="del";
+  deleteButton.classList.add('btn')
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.classList.add('del-img')
   deleteButton.appendChild(deleteButtonImg);
 
 
@@ -83,13 +92,15 @@ var editTask=function(){
   var listItem=this.parentNode;
 
   var editInput=listItem.querySelector('input[type=text]');
+  editInput.classList.toggle('hide')
   var label=listItem.querySelector("label");
+  label.classList.toggle('hide')
   var editBtn=listItem.querySelector(".edit");
-  var containsClass=listItem.classList.contains("editMode");
-  //If class of the parent is .editmode
+  var containsClass=listItem.classList.contains("edit-mode");
+  //If class of the parent is .edit-mode
   if(containsClass){
 
-  //switch to .editmode
+  //switch to .edit-mode
   //label becomes the inputs value.
   label.innerText=editInput.value;
   editBtn.innerText="Edit";
@@ -98,8 +109,8 @@ var editTask=function(){
   editBtn.innerText="Save";
   }
 
-  //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  //toggle .edit-mode on the parent.
+  listItem.classList.toggle("edit-mode");
 };
 
 
@@ -121,6 +132,8 @@ var taskCompleted=function(){
 
   //Append the task list item to the #completed-tasks
   var listItem=this.parentNode;
+  let textListItem = listItem.querySelector('.task')
+  textListItem.classList.add('completed')
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
 
@@ -132,7 +145,10 @@ var taskIncomplete=function(){
 //Mark task as incomplete.
   //When the checkbox is unchecked
   //Append the task list item to the #incompleteTasks.
+
   var listItem=this.parentNode;
+  let textListItem = listItem.querySelector('.task')
+  textListItem.classList.toggle('completed')
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem,taskCompleted);
 }
@@ -157,7 +173,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 //select ListItems children
   var checkBox=taskListItem.querySelector("input[type=checkbox]");
   var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var deleteButton=taskListItem.querySelector(".del");
 
 
   //Bind editTask to edit button.
